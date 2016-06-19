dc17444a4347b54c26c79de3713d2da35fc3e0fb
iostreamer-X <iostreamer007@gmail.com>
2016-06-20 04:25:10 +0530

    second test

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var express = require('express');
-var fs = require('fs');
+var http = require('http');
+var WebSocketServer = require('ws').Server
 
-var app = express()
+var app=express()
 
-app.get('/',(req,res)=>{
-  fs.createReadStream("test.txt")
-    .pipe(res)
-})
+var server = http.createServer(app)
+server.listen(process.env.PORT || 5000)
 
-app.listen(process.env.PORT || 5000,()=>{
-  fs.writeFile("test.txt","successful!",(e)=>{})
+var ws = new WebSocketServer({server:server})
+
+ws.on('connection',(conn)=>{
+  console.log('Connection established');
+  conn.send('io')
 })
