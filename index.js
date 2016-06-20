24c5b085f1faa2ae38952f56efe498d4e3b5a181
iostreamer-X <iostreamer007@gmail.com>
2016-06-20 21:13:09 +0530

    third test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app=express()
 var response;
 
 app.get('/',(req,res)=>{
-  res.writeHead(200, {'Content-Type': 'audio/mpeg'})
+  res.writeHead(200, {'Content-Type': 'audio/mpeg','Cache-Control': 'no-cache'})
   res.write('ok')
   response=res
   response.on('drain',()=>{console.log('draaaaaaaaaaaaaaaaaaaaaiiiiiin!!!!!')})
