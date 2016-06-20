2a78ad359d938b22c755c613750e041debb55e7e
iostreamer-X <iostreamer007@gmail.com>
2016-06-20 23:42:48 +0530

    personal

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ var http = require('http');
 var WebSocketServer = require('ws').Server
 
 var app=express()
-var response;
+var listeners = []
 
 app.get('/',(req,res)=>{
   res.writeHead(200, {'Content-Type': 'audio/mpeg','Cache-Control': 'no-cache'})
-  response=res
-  response.on('drain',()=>{console.log('draaaaaaaaaaaaaaaaaaaaaiiiiiin!!!!!')})
+  listeners.push(res)
+  res.on('close',()=>{
+    var index = listeners.indexOf(res)
+    listeners.splice(index,1)
+  })
 })
 
 
@@ -19,9 +22,8 @@ var ws = new WebSocketServer({server:server})
 
 ws.on('connection',(conn)=>{
   conn.on('message',(data)=>{
-    if(response!=undefined)
-      var r = response.write(data)
-      if(!r)
-        console.log('couldn\'t write');
+    for(var listener of listeners){
+      listener.write(data)
+    }
   })
 })
